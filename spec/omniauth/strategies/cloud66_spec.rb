bd60f6465817895c33188ac7bec5d99dfc36c5a0
nikolalsvk <nikolaseap@gmail.com>
2017-11-03 11:22:23 +0100

    Fix specs

--- spec/omniauth/strategies/cloud66_spec.rb
+++ spec/omniauth/strategies/cloud66_spec.rb
@@ -1,24 +1,21 @@
 require 'spec_helper'
 
 describe OmniAuth::Strategies::Cloud66 do
-
   subject do
     OmniAuth::Strategies::Cloud66.new({})
   end
 
   context "client options" do
     it 'should have correct site' do
-      subject.options.client_options.site.should eq("https://www.cloud66.com/api/2")
+      subject.options.client_options.site.should eq("https://app.cloud66.com/api/2")
     end
 
     it 'should have correct authorize url' do
-      subject.options.client_options.authorize_url.should eq("https://www.cloud66.com/oauth/authorize")
+      subject.options.client_options.authorize_url.should eq("https://app.cloud66.com/oauth/authorize")
     end
 
     it 'should have correct token url' do
-      subject.options.client_options.token_url.should eq("https://www.cloud66.com/oauth/token")
+      subject.options.client_options.token_url.should eq("https://app.cloud66.com/oauth/token")
     end
-
   end
-
 end
